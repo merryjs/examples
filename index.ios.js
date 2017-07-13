e47798139161a60d4fa6da014e1789e8efc7dbb2
bang <sqibang@gmail.com>
2017-07-14 01:36:26 +0700

    chore: add toast example

---
+++ index.ios.js
@@ -0,0 +1,55 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react'
+import { AppRegistry, StyleSheet, Text, View, Button } from 'react-native'
+import Toast from '@merryjs/toast'
+console.log(Toast)
+export default class MerryExamples extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+        <Button
+          title="Show Toast"
+          onPress={() => Toast.show('A merry toast', Toast.SHORT)}
+        />
+        <Button
+          title="Show Toast With Gravity"
+          onPress={() => Toast.showWithGravity('A merry toast', Toast.SHORT,Toast.CENTER)}
+        />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+})
+
+AppRegistry.registerComponent('MerryExamples', () => MerryExamples)

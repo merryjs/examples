deda83604f48682ba54bed757bdfcfb0a9065d5f
bang <sqibang@gmail.com>
2017-07-23 01:26:29 +0700

    chore: update demo

--- index.ios.js
+++ index.ios.js
@@ -9,6 +9,7 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
+  ScrollView,
   View,
   Button,
   Image,
@@ -24,20 +25,18 @@ const { MerryPhotoViewer } = NativeModules;
 const cats = [
   "https://c1.staticflickr.com/8/7625/16631849053_db25684173_k.jpg",
   "https://c1.staticflickr.com/6/5598/14934282524_577a904d2b_k.jpg",
-  "https://c1.staticflickr.com/8/7596/17021131801_fbd8f2b71a_k.jpg"
+  "https://c1.staticflickr.com/8/7596/17021131801_fbd8f2b71a_k.jpg",
+  "https://images.pexels.com/photos/45170/kittens-cat-cat-puppy-rush-45170.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb",
+  "https://images.pexels.com/photos/142615/pexels-photo-142615.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb",
+  "https://images.pexels.com/photos/82072/cat-82072.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb",
+  // "https://images.pexels.com/photos/248261/pexels-photo-248261.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"
 ];
 export default class MerryExamples extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{"\n"}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Text style={styles.welcome}>Merry Native !</Text>
+
         <Button
           title="Show Toast"
           onPress={() => Toast.show("A merry toast", Toast.SHORT)}
@@ -47,15 +46,25 @@ export default class MerryExamples extends Component {
           onPress={() =>
             Toast.showWithGravity("A merry toast", Toast.SHORT, Toast.CENTER)}
         />
-        <View
-          style={{
-            flex: 1
+        <ScrollView
+          style={{}}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{
+            justifyContent: "center",
+            flex: 1,
+            flexDirection: "row",
+            flexWrap: "wrap"
           }}
         >
           {cats.map((cat, index) =>
             <TouchableOpacity
               key={cat}
-              onPress={() => MerryPhotoViewer.show(cats, index)}
+              style={{
+                width: 200,
+                height: 200
+              }}
+              onPress={() =>
+                MerryPhotoViewer.show({ photos: cats, initial: index })}
             >
               <Image
                 style={{ width: 200, height: 200 }}
@@ -63,7 +72,7 @@ export default class MerryExamples extends Component {
               />
             </TouchableOpacity>
           )}
-        </View>
+        </ScrollView>
       </View>
     );
   }
@@ -72,8 +81,6 @@ export default class MerryExamples extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
     backgroundColor: "#F5FCFF"
   },
   welcome: {
